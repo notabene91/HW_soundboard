e00f4a597f51b8f3b7044161fac4a2444ce50513
Александр Шабалин <notabene1991@yandex.ru>
2020-08-20 20:05:19 +0300

    convert to ogg, enable pause

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const audio = document.querySelector('.container__track');
 items.forEach((item) => {
   item.childNodes[1].volume = 0.3;
   item.addEventListener('click', () => {
-    item.childNodes[1].play();
-    item.childNodes[3].classList.remove('container__image_gray');
+    if (item.childNodes[1].paused) {
+      item.childNodes[1].play();
+      item.childNodes[3].classList.remove('container__image_gray');
+    }
+    else {
+      item.childNodes[1].pause();
+    }
   })
+
   item.childNodes[1].addEventListener('ended', () => {
     item.childNodes[3].classList.add('container__image_gray');
   })
